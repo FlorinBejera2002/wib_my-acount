1864fa0a5ed2c62dee1e3b5fee7bfcf71d948801
FlorinBejera2002 <florinpetru0306@gmail.com>
2026-05-04 15:46:53 +0300

    improve: close hamburgher button

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -196,7 +196,7 @@ const Sidebar = React.forwardRef<
           <SheetContent
             data-sidebar="sidebar"
             data-mobile="true"
-            className="w-[--sidebar-width] bg-sidebar p-0 text-sidebar-foreground"
+            className="w-[--sidebar-width] bg-sidebar p-0 text-sidebar-foreground [&>button]:top-2.5"
             style={
               {
                 '--sidebar-width': SIDEBAR_WIDTH_MOBILE
