802d722084267d3bbd23772a8fcec9d54fd5f7c7
FlorinBejera2002 <florinpetru0306@gmail.com>
2026-05-04 15:51:42 +0300

    improve: size pages

--- src/components/layout/app-layout.tsx
+++ src/components/layout/app-layout.tsx
@@ -9,7 +9,7 @@ export function AppLayout() {
       <AppSidebar />
       <SidebarInset className="bg-zinc-50/40">
         <AppHeader />
-        <div className="flex-1 overflow-y-auto p-4 lg:p-6">
+        <div className="flex-1 overflow-y-auto p-4 pt-8 lg:p-8 lg:pt-12">
           <Outlet />
         </div>
       </SidebarInset>
